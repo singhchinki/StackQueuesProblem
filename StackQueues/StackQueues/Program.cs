38035b9cb82d2c56d2295d4fec3827b8b4fad48a
chinki kumari singh <singhchinkikumari@gmail.com>
2022-06-01 21:47:47 +0530

    [chinki]Ability to create a Stack

---
+++ StackQueues/StackQueues/Program.cs
@@ -0,0 +1,5 @@
+﻿StackQueues.StackList linkedListStack = new StackQueues.StackList();
+linkedListStack.push(70);
+linkedListStack.push(30);
+linkedListStack.push(56);
+linkedListStack.Display();
---
+++ StackQueues/StackQueues/StackList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueues
+{
+    class StackList
+    {
+        public Node top;
+        public StackList()
+        {
+            this.top = null;
+        } 
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(" Push value in list " + value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Inserted into the stack Successfully  " + temp.data);
+                temp = temp.next;
+            }
+        }
+
+    }
+}
