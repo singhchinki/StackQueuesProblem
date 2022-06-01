4a5937f135ab8007516812d72660ecbd1fc685cd
chinki kumari singh <singhchinkikumari@gmail.com>
2022-06-01 23:59:08 +0530

    [chinki]Ability to create Queue

--- StackQueues/StackQueues/QueueList.cs
+++ StackQueues/StackQueues/QueueList.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace StackQueues
 {
-    internal class QueueList
+    class QueueList
     {
+        Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("\n inserted into queue = " + node.data);
+        }
+
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                head = head.next;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
